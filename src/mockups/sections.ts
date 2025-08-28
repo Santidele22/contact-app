51506b52fd5dbcc6f267afd4df301eddd12d4751
santiidele22 <santiidele21@gmail.com>
2025-08-28 00:11:59 -0300

    Cambie variables css y agregue seccion de contactos

---
+++ src/mockups/sections.ts
@@ -0,0 +1,18 @@
+interface linkSection {
+    text: string
+    title: string
+    href: string
+}
+
+export const sections: linkSection[] = [
+  {
+    text: "Ver mi portafolio",
+    title: "Portafolio",
+    href: "https://tu-portafolio.com"
+  },
+  {
+    text: "Agendá una llamada",
+    title: "¿Necesitás una página?",
+    href: "https://calendly.com/tu-usuario"
+  }
+];
