bcb56ce94697ee3c5939e8642fdf3ac103846687
santiidele22 <santiidele21@gmail.com>
2025-08-29 11:19:06 -0300

    Agregue links correspondientes y cambie el logo

--- src/mockups/sections.ts
+++ src/mockups/sections.ts
@@ -1,18 +1,18 @@
 interface linkSection {
-    text: string
-    title: string
-    href: string
+  text: string;
+  title: string;
+  href: string;
 }
 
 export const sections: linkSection[] = [
   {
     text: "Ver mi portafolio",
     title: "Portafolio",
-    href: "https://tu-portafolio.com"
+    href: "https://portafolio-v3-one.vercel.app/",
   },
   {
     text: "Agendá una llamada",
     title: "¿Necesitás una página?",
-    href: "https://calendly.com/tu-usuario"
-  }
+    href: "https://calendly.com/santiidele21/new-meeting",
+  },
 ];
--- src/mockups/social-media.ts
+++ src/mockups/social-media.ts
@@ -1,36 +1,33 @@
-import instagramImage from "@assets/icons/instagram-icon.svg"
-import calendlyImage from "@assets/icons/calendly.svg"
-import githubImage from "@assets/icons/github-icon.svg"
-import linkedinImage from "@assets/icons/linkedin-icon.svg"
-
-
+import instagramImage from "@assets/icons/instagram-icon.svg";
+import calendlyImage from "@assets/icons/calendly.svg";
+import githubImage from "@assets/icons/github-icon.svg";
+import linkedinImage from "@assets/icons/linkedin-icon.svg";
 
 interface links {
-    text: string
-    icon: ImageMetadata
-    href: string
+  text: string;
+  icon: ImageMetadata;
+  href: string;
 }
 export const socialMediaLinks: links[] = [
-    {
-        text: "Instagram",
-        icon: instagramImage,
-        href: "https://www.instagram.com/"
-    },
+  {
+    text: "Instagram",
+    icon: instagramImage,
+    href: "https://www.instagram.com/delebecq22/",
+  },
 
-    {
-        text: "LinkedIn",
-        icon: linkedinImage,    
-        href: "https://www.linkedin.com/"
-    },
-    {
-        text: "GitHub",
-        icon: githubImage,
-        href: "https://github.com/"
-    },
-        {
-        text: "Calendly",
-        icon: calendlyImage,
-        href: "https://calendly.com/"
-    },
-   
-]
+  {
+    text: "LinkedIn",
+    icon: linkedinImage,
+    href: "https://www.linkedin.com/in/santiago-delebecq-8630a2215/",
+  },
+  {
+    text: "GitHub",
+    icon: githubImage,
+    href: "https://github.com/Santidele22",
+  },
+  {
+    text: "Calendly",
+    icon: calendlyImage,
+    href: "https://calendly.com/santiidele21/new-meeting",
+  },
+];
